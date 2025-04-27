4418e2e04a8ecccbb4db160b4285818f44d9561a
Richard Tang <code.monkey.ricky@gmail.com>
2025-04-26 20:04:11 -0700

    snapshot

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all:
 	ghc Amq.hs -package stompl -package utf8-string -package mime
+	ghc Consumer.hs -package stompl -package utf8-string -package mime
---
+++ Producer.hs
@@ -0,0 +1,36 @@
+import           Codec.MIME.Type                 (nullType)
+import           Control.Concurrent              (threadDelay)
+import           Control.Monad                   (forever)
+import qualified Data.ByteString.UTF8            as U
+import           Network.Mom.Stompl.Client.Queue
+import           Network.Socket                  (withSocketsDo)
+import           System.Environment              (getArgs)
+
+data Ping
+  = Ping
+  | Pong
+  deriving (Show)
+
+main :: IO ()
+main = do
+  os <- getArgs
+  case os of
+    [q] -> withSocketsDo $ producer q
+    _   -> putStrLn "Usage: producer <queue-name>"
+
+producer :: String -> IO ()
+producer qn =
+  withConnection
+    "localhost"
+    61616
+    [OAuth "artemis" "artemis", OHeartBeat (5000, 5000)]
+    [] $ \c -> do
+    let oconv = return . U.fromString . show
+    withWriter c "Q-OUT" qn [] [] oconv $ \outQ -> do
+      putStrLn $ "Producer started sending to queue: " ++ qn
+      forever $ do
+        writeQ outQ nullType [] Pong
+        -- putStrLn "Sent Pong"
+        -- threadDelay 10000000 -- 10 second delay between messages
+  where
+    Pong = Pong -- Your Ping/Pong data type
