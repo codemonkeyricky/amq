6a35e4377b9bf9474dfe3ffc4bdee2f9b9c6bee3
Richard Tang <code.monkey.ricky@gmail.com>
2025-04-27 21:04:40 -0700

    snapshot

--- Common.hs
+++ Common.hs
@@ -3,11 +3,16 @@
 
 module Common where
 
-import           Data.Aeson   (FromJSON, ToJSON, decode, encode)
+import           Data.Aeson           (FromJSON, ToJSON, decode, encode)
+import qualified Data.ByteString.Lazy as DBL
+import qualified Data.ByteString.UTF8 as UTF8
 import           GHC.Generics
 
 data Event = Event
-  { id     :: Integer
+  { uid    :: Integer
   , event  :: String
   , action :: String
   } deriving (Show, Eq, Generic, ToJSON, FromJSON)
+
+serialize :: Event -> UTF8.ByteString
+serialize = UTF8.fromString . show . encode
--- Producer.hs
+++ Producer.hs
@@ -18,22 +18,24 @@ main :: IO ()
 main = do
   os <- getArgs
   case os of
-    [q] -> withSocketsDo $ producer q
-    _   -> putStrLn "Usage: producer <queue-name>"
+    [q] ->
+      withSocketsDo
+        $ producer q Event {uid = 0, event = "security", action = "time"}
+    _ -> putStrLn "Usage: producer <queue-name>"
 
-producer :: String -> IO ()
-producer qn =
+producer :: String -> Event -> IO ()
+producer qn evt =
   withConnection
     "localhost"
     61616
     [OAuth "artemis" "artemis", OHeartBeat (5000, 5000)]
     [] $ \c -> do
-    let oconv = return . B.pack
+    let oconv = return . serialize
     withWriter c "Q-OUT" qn [] [] oconv $ \outQ -> do
       putStrLn $ "Producer started sending to queue: " ++ qn
-      forM_ [1 .. 10000] $ \_ -> do
-        writeQ outQ nullType [] "Pong"
+      forM_ [1 .. 1] $ \_ -> do
+        writeQ outQ nullType [] evt
       -- forever $ do
       --   writeQ outQ nullType [] "Pong"
       -- replicateM_ 10000 $ writeQ outQ nullType [] "Pong"
-      threadDelay 1000000
+      threadDelay 100000
