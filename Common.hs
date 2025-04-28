f9a27f707ba7131685d64a324ddabe26cffa34ee
Richard Tang <code.monkey.ricky@gmail.com>
2025-04-27 21:18:31 -0700

    snapshot

--- Common.hs
+++ Common.hs
@@ -4,6 +4,7 @@
 module Common where
 
 import           Data.Aeson           (FromJSON, ToJSON, decode, encode)
+import qualified Data.ByteString      as BS
 import qualified Data.ByteString.Lazy as DBL
 import qualified Data.ByteString.UTF8 as UTF8
 import           GHC.Generics
@@ -14,5 +15,8 @@ data Event = Event
   , action :: String
   } deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
-serialize :: Event -> UTF8.ByteString
-serialize = UTF8.fromString . show . encode
+serialize :: Event -> BS.ByteString
+serialize = BS.concat . DBL.toChunks . encode
+
+deserialize :: BS.ByteString -> Maybe Event
+deserialize = decode . DBL.fromStrict
